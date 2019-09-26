39e00e0fd45e799ebd34d0f313dd7eeaa5003ebc
PettoDavida <maximilian.widman@gmail.com>
2019-09-26 13:22:54 +0200

    Jag har skapat den nederländska flaggan och jobbar på att fixa så att man ska kunna byta flagga mellan flaggorna

--- Java/Flags and Geometry/src/sample/Controller.java
+++ Java/Flags and Geometry/src/sample/Controller.java
@@ -33,6 +33,83 @@ public class Controller {
 
     }
 
+    public static FlagClass Netherlands() {
+        FlagClass flagga = new FlagClass();
+
+        Rectangle vStripe = new Rectangle();
+
+        vStripe.setWidth(600);
+        vStripe.setHeight(100);
+        vStripe.setTranslateX(0);
+        vStripe.setFill(Color.DARKRED);
+
+        Rectangle vStripe2 = new Rectangle();
+
+        vStripe2.setWidth(600);
+        vStripe2.setHeight(100);
+        vStripe2.setTranslateY(100);
+        vStripe2.setFill(Color.WHITE);
+
+        Rectangle vStripe3 = new Rectangle();
+
+        vStripe3.setWidth(600);
+        vStripe3.setHeight(100);
+        vStripe3.setTranslateY(200);
+        vStripe3.setFill(Color.BLUE);
+
+        Group root = new Group(vStripe, vStripe2, vStripe3);
+
+        Scene scene = new Scene(root, 600, 300, Color.LAVENDER);
+
+        flagga.getChildren().addAll(vStripe, vStripe2, vStripe3);
+
+        return flagga;
+    }
+
+    public static FlagClass Switzerland() {
+        FlagClass flagga = new FlagClass();
+
+        Rectangle vStripe = new Rectangle();
+
+        vStripe.setWidth(600);
+        vStripe.setHeight(300);
+        vStripe.setTranslateX(0);
+        vStripe.setFill(Color.RED);
+
+        Group root = new Group(vStripe);
+
+        Scene scene = new Scene(root, 600, 300, Color.LAVENDER);
+
+        flagga.getChildren().addAll(vStripe);
+
+        return flagga;
+
+    }
+
+    public static FlagClass Ukraine() {
+        FlagClass flagga = new FlagClass();
+
+        Rectangle vStripe = new Rectangle();
+
+        vStripe.setWidth(600);
+        vStripe.setHeight(150);
+        vStripe.setTranslateX(0);
+        vStripe.setFill(Color.BLUE);
+
+        Rectangle vStripe2 = new Rectangle();
+
+        vStripe2.setWidth(600);
+        vStripe2.setHeight(150);
+        vStripe2.setTranslateY(150);
+        vStripe2.setFill(Color.YELLOW);
+
+        // Group root = new Group(vStripe, vStripe2);
+
+        flagga.getChildren().addAll(vStripe, vStripe2);
+
+        return flagga;
+
+    }
 
 
 }
--- Java/Flags and Geometry/src/sample/Main.java
+++ Java/Flags and Geometry/src/sample/Main.java
@@ -1,8 +1,20 @@
 package sample;
         import javafx.application.Application;
+        import javafx.beans.value.ChangeListener;
+        import javafx.beans.value.ObservableValue;
+        import javafx.event.ActionEvent;
+        import javafx.event.EventHandler;
         import javafx.scene.Group;
         import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.RadioButton;
+        import javafx.scene.control.Toggle;
+        import javafx.scene.control.ToggleGroup;
+        import javafx.scene.layout.BorderPane;
+        import javafx.scene.layout.StackPane;
+        import javafx.scene.layout.VBox;
         import javafx.scene.paint.Color;
+        import javafx.scene.shape.Rectangle;
         import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,12 +22,31 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Group root = new Group();
+        StackPane stack = new StackPane();
+        BorderPane root = new BorderPane();
+        Rectangle bar = new Rectangle();
+        bar.setWidth(150);
+        bar.setHeight(500);
+        bar.setFill(new Color(166/255f,36/255f,75/255f,1));
+        stack.getChildren().add(bar);
+        VBox buttons = new VBox();
+        ToggleGroup radio = new ToggleGroup();
+        radio.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("wooh");
+        });
+        RadioButton sweden = new RadioButton("Sweden");
+        RadioButton netherlands = new RadioButton("Netherlands");
+        sweden.setToggleGroup(radio);
+        netherlands.setToggleGroup(radio);
+        stack.getChildren().add(buttons);
+        buttons.getChildren().add(sweden);
+        buttons.getChildren().add(netherlands);
+        root.setLeft(stack);
         Scene scene = new Scene(root, 500, 500, Color.LAVENDER);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        root.getChildren().add(Controller.Sweden());
+
 
 
     }
