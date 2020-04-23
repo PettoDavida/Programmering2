02d40a38c755757e8441dc11ccd48a0a9dca6811
PettoDavida <pettodavida@gmail.com>
2020-04-23 13:28:12 +0200

    Början på Slutprojekt
    Börjat koda

---
+++ C-Sharp/Slutprojekt Poker/Slutprojekt Poker/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutprojekt_Poker
+{
+    class Bot
+    {
+        static void botmove()
+        {
+            Random random = new Random();
+
+            int move = random.Next(0, 8);
+
+            if ()
+            {
+
+            }
+        }
+    }
+}
---
+++ SlutProjekt Java/src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Controller {
+
+
+
+    private static String[] suits = { "hearts", "spades", "diamonds", "clubs" };
+    private static String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public void start(){
+
+    }
+}
